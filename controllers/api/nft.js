7aaa68eefee3c1c0cde200d29cfb8cd9a0fae484
illiadimension <illiadimension@github.com>
2023-03-02 09:59:20 +0200

    add avatar renderer

---
+++ controllers/api/nft.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const nftHelper = require('../../helpers/dna-parser')
+
+class NFTController {
+  async get (req, res, next) {
+    const { type, id } = req.params
+    const { width = 500, height = 500, glow = 'true' } = req.query;
+    if (!type || !id) {
+      res.status(404).json({ error: 'Wrong format' })
+    }
+    if (type) {
+      const nft = await nftHelper.get(type, id);
+
+      console.log('nft', nft);
+      if (nft) {
+        nft['glow_color'] = nft.primary_color.replace(')', ', 0.3)').replace('rgb', 'rgba');
+        nft['hair_styles'] = 'asymmetrical'
+        res.setHeader('Content-Type', 'image/svg+xml');
+        res.render(`layouts/${type}.hbs`, {
+          layout: `${type}.hbs`,
+          ...nft,
+          glow,
+          width: width,
+          height: height
+        })
+      } else {
+        res.status(404).json({ error: 'File not found' })
+      }
+      
+    } else {
+      res.status(404).json({ error: 'File not found' })
+    }
+  }
+}
+
+module.exports = new NFTController()
